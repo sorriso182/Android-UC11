1b04a9ba493624a7ae019ad20f91e08f35aafa77
Sala 302B <sala302b@ceines.com.br>
2019-08-28 20:55:15 -0300

    botao funfando

--- Android-01/HelloWorld/app/src/main/java/br/senac/es/helloworld/MainActivity.java
+++ Android-01/HelloWorld/app/src/main/java/br/senac/es/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package br.senac.es.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+
+        final TextView textMetros = (TextView) findViewById(R.id.textMetros);
+
+        textMetros.setText("Olá Mundo");
+
+        //final TextView textAltura = (TextView) findViewById(R.id.textAltura);
+
+        // textAltura.setText("asaasaa");
+
+        //final TextView textPes = (TextView) findViewById(R.id.textPes);
+
     }
 }
